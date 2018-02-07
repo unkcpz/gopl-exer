9bf21be15ef6f9e641480388c85d5f660eeb5c8f
Jason.Yu <morty.yu@yahoo.com>
2018-02-07 16:58:06 +0800

    ch1/echo3

---
+++ ch1/echo2/echo.go
@@ -0,0 +1,16 @@
+// Echo2 prints cmd arguments by looping range
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ ch1/echo3/echo.go
@@ -0,0 +1,12 @@
+// Echo3 prints cmd arguments using string.Join()
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
