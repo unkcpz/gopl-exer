23d135af3bcc9e8e17783af27b0e89db28664c08
Jason.Yu <morty.yu@yahoo.com>
2018-02-08 16:38:14 +0800

    finish ex1_6

---
+++ ch1/lissajous/lissajous.go
@@ -0,0 +1,50 @@
+// make a lissajous GIF
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		nframes = 64
+		delay   = 8   //delay between frames in 10ms units
+		size    = 100 // canvas size
+	)
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	freq := rand.Float64()
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+		phase += 0.1
+	}
+	gif.EncodeAll(out, &anim)
+}
