7d10f23c249d43d33a49c7afa1815b6d6d6695ba
Jason.Yu <morty.yu@yahoo.com>
2018-02-27 13:39:31 +0800

    ex2_3 done

---
+++ exch2/ex2_3/popcount.go
@@ -0,0 +1,29 @@
+// Methods to count the population of integral sets
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountLoop(x uint64) int {
+	var sum int
+	for i := uint(0); i < 8; i++ {
+		sum += int(pc[byte(x>>(i*8))])
+	}
+	return sum
+}
